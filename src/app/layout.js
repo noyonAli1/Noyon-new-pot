a71207d409b21a18eb6559423a83dbd007483312
noyonAli1 <noyon.ali713703@gmail.com>
2026-05-10 00:12:51 +0600

    add to code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/shared/Navbar";
+import About from "@/components/shared/About";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,12 +22,13 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
+      theme="light"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">
+      <body >
         <Navbar></Navbar>
         {children}
-        
+         <About></About>
         </body>
     </html>
   );
